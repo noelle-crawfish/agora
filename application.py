a58600438024dbc3d5f6e2c034d2044839dc849b
noelle-crawfish <noellegc3@gmail.com>
2018-11-17 14:44:30 -0800

    registration functionality

--- application.py
+++ application.py
@@ -5,6 +5,8 @@ from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
+from helpers import *
+
 app = Flask(__name__)
 
 # Check for environment variable
@@ -21,8 +23,58 @@ engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
-def index() :
-    return render_template("anon_template.html")
+def index():
+    return render_template("user_template.html")
+
+@app.route("/login_requests", methods=["POST"])
+def process_login_request():
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    print(username)
+    print(password)
+
+    return redirect("/")
+
+@app.route("/register_requests", methods=["POST"])
+def process_register_request():
+    username = request.form.get("username")
+    password = request.form.get("password")
+    confirm = request.form.get("confirm-password")
+    agreed = request.form.get("terms-of-service")
+
+    if not agreed:
+        return render_template("register.html", has_error=True, error="Please agree to our terms of service")
+
+    if not password == confirm:
+        return render_template("register.html", has_error=True, error="Passwords must match")
+    
+    # Check to see if user already exists
+    old_user = db.execute("SELECT * FROM users WHERE username = :username", {"username":username}).fetchone()
+    if not user is None:
+        return render_template("register.html", has_error=True, error="That username is already taken")
+
+    hashed = hash(password)
+    db.execute("INSERT INTO users (username, password) VALUES (:username, :password)", {"username":username, "password":hashed})
+    db.commit()
+    session["username"] = username
+    return redirect("/dashboard")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return "dashboard"
+
+@app.route("/termsofservice")
+def tos():
+    return "They don't exist just please don't sue us."
 
 if __name__ == "__main__":
     threading.Thread(target=app.run).start()
